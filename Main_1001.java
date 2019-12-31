d6a87f1968f345ee78b5f8b8ee4d9cb1f4a72634
rkdud0295 <59392236+rkdud0295@users.noreply.github.com>
2020-01-01 03:14:58 +0900

    Add files via upload

---
+++ Main_1001.java
@@ -0,0 +1,27 @@
+package gayoung;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+
+
+
+public class Main_1001 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+String word = br.readLine();
+int a = 0;
+int b = 0;
+StringTokenizer st = new StringTokenizer(word);
+a = Integer.parseInt(st.nextToken());
+b = Integer.parseInt(st.nextToken());
+int sum = a - b;
+System.out.print(sum);
+	}
+
+}
