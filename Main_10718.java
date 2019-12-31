b53d6c9dcb94704194673697c914ebb640e37354
rkdud0295 <59392236+rkdud0295@users.noreply.github.com>
2019-12-31 22:32:59 +0900

    Add files via upload
    2019.12.31 Solved

---
+++ Main_10718.java
@@ -0,0 +1,10 @@
+package gayoung;
+public class Main_10718 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("강한친구 대한육군");
+		System.out.print("\n");
+		System.out.print("강한친구 대한육군");
+	}
+}
---
+++ Main_2557.java
@@ -0,0 +1,18 @@
+package gayoung;
+
+public class Main_2557 {
+
+	public static void main(String[] args) {
+		//  //주석
+		// "" 문자열
+		// '' 문자
+		// \n 개행
+		// = 값을 넣는다
+		// == 같다
+		// != 다르다
+		// ! 아니다(not)
+		
+		System.out.print("Hello World!");
+
+	}
+}
